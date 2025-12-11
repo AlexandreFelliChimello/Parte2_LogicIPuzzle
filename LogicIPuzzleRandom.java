4fed1b7d16ecc999c888d48d3c324b2a4eb6904e
Ruan Vieira <ruanvieira.pimentel@gmail.com>
2025-12-11 20:19:51 +0000

    LogicIPuzzleRandom feito

--- LogicIPuzzleRandom.java
+++ LogicIPuzzleRandom.java
@@ -1,16 +1,32 @@
-public class LogicIPuzzleRandom{
-    public static void main (String[] args){
-    public int rows() / columns(){
+public class LogicIPuzzleRandom {
+    private int rows;
+    private int cols;
+    private LogicIPuzzle[] puzzles;
+    private int nextIndex;
 
+    public LogicIPuzzleRandom(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.puzzles = new LogicIPuzzle[0];
+        this.nextIndex = 0;
     }
-    public boolean hasNextPuzzle(){
 
+    public int rows() {
+        return rows;
     }
-    public LogicIPuzzle nextPuzzle(){
 
+    public int columns() {
+        return cols;
     }
-    }
-    public LogicIPuzzleRandom(int rows, int cols){
 
-}
+    public boolean hasNextPuzzle() {
+        return nextIndex < puzzles.length;
+    }
+
+    public LogicIPuzzle nextPuzzle() {
+        if (!hasNextPuzzle()) {
+            return null;
+        }
+        return puzzles[nextIndex++];
+    }
 }
